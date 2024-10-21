4823002771249056f695efbf3f09e070812e408a
xiemenwaidao <menxielijiao@gmail.com>
2024-10-22 01:31:03 +0900

    💚 pnpm run format（2）

--- src/stores/useGLStore.ts
+++ src/stores/useGLStore.ts
@@ -4,28 +4,28 @@ import { quadsAlmostEqual, tripletsAlmostEqual } from "@utils/helper";
 import { create } from "zustand";
 
 interface State {
-    position: Triplet;
-    quaternion: Quad;
+  position: Triplet;
+  quaternion: Quad;
 }
 
 interface Actions {
-    setPosition: (position: Triplet) => void;
-    setQuaternion: (position: Quad) => void;
+  setPosition: (position: Triplet) => void;
+  setQuaternion: (position: Quad) => void;
 }
 
 export const useCubeStore = create<State & Actions>((set, get) => ({
-    position: [0, 0, 0],
-    setPosition: (newPosition: Triplet) => {
-        const currentPosition = get().position;
-        if (!tripletsAlmostEqual(currentPosition, newPosition, epsilon)) {
-            set({ position: newPosition });
-        }
-    },
-    quaternion: [0, 0, 0, 0],
-    setQuaternion: (newQuaternion: Quad) => {
-        const currentQuaternion = get().quaternion;
-        if (!quadsAlmostEqual(currentQuaternion, newQuaternion, epsilon)) {
-            set({ quaternion: newQuaternion });
-        }
-    },
+  position: [0, 0, 0],
+  setPosition: (newPosition: Triplet) => {
+    const currentPosition = get().position;
+    if (!tripletsAlmostEqual(currentPosition, newPosition, epsilon)) {
+      set({ position: newPosition });
+    }
+  },
+  quaternion: [0, 0, 0, 0],
+  setQuaternion: (newQuaternion: Quad) => {
+    const currentQuaternion = get().quaternion;
+    if (!quadsAlmostEqual(currentQuaternion, newQuaternion, epsilon)) {
+      set({ quaternion: newQuaternion });
+    }
+  },
 }));
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,9 +1,9 @@
 export const lightParams = {
-    cubeColor: "#006CAC",
-    bgColor: "#f5f3ff",
+  cubeColor: "#006CAC",
+  bgColor: "#f5f3ff",
 };
 export const darkParams = {
-    cubeColor: "#A33206",
-    bgColor: "#212737",
+  cubeColor: "#A33206",
+  bgColor: "#212737",
 };
 export const epsilon = 1e-2;
