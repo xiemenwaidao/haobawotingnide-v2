6d4bb169fedcc1b89985509fdd77cd25fbeb60ca
xiemenwaidao <menxielijiao@gmail.com>
2024-10-22 01:24:58 +0900

    💚 ciで怒られた

--- src/components/GL/Lights.tsx
+++ src/components/GL/Lights.tsx
@@ -1,70 +1,68 @@
-import { useHelper } from "@react-three/drei/native";
-import React from "react";
 import { useCallback, useEffect, useRef } from "react";
 import { useCubeStore } from "stores/useGLStore";
-import { Quaternion, SpotLight, SpotLightHelper, Vector3 } from "three";
+import { Quaternion, SpotLight, Vector3 } from "three";
 
 const Lights = () => {
-    const ref = useRef<SpotLight>(null!);
-    // useHelper(ref, SpotLightHelper);
+  const ref = useRef<SpotLight>(null!);
+  // useHelper(ref, SpotLightHelper);
 
-    // サイコロの情報を取得
-    const cubePosition = useCubeStore(state => state.position);
-    const cubeQuaternion = useCubeStore(state => state.quaternion);
+  // サイコロの情報を取得
+  const cubePosition = useCubeStore(state => state.position);
+  const cubeQuaternion = useCubeStore(state => state.quaternion);
 
-    // useEffect依存ハック
-    const prevCubePosition = useRef(cubePosition);
-    const prevCubeQuaternion = useRef(cubeQuaternion);
+  // useEffect依存ハック
+  const prevCubePosition = useRef(cubePosition);
+  const prevCubeQuaternion = useRef(cubeQuaternion);
 
-    const updateLight = useCallback(() => {
-        // console.log("update light");
-        if (!ref.current) return;
+  const updateLight = useCallback(() => {
+    // console.log("update light");
+    if (!ref.current) return;
 
-        // サイコロの上面を算出
-        const upFaceNormal = new Vector3(0, 1, 0).applyQuaternion(
-            new Quaternion(...cubeQuaternion)
-        );
-
-        // 位置算出
-        const lightOffset = new Vector3(2, 4, 2);
-        const lightPosition = new Vector3()
-            .addVectors(new Vector3(...cubePosition), upFaceNormal)
-            .add(lightOffset);
-
-        ref.current.target.position.set(...cubePosition);
-        ref.current.position.set(...lightPosition.toArray());
-        ref.current.target.updateMatrixWorld();
-    }, [cubePosition, cubeQuaternion]);
-
-    // 最初の一発だけ
-    useEffect(() => {
-        updateLight();
-    }, []);
-
-    useEffect(() => {
-        updateLight();
-
-        prevCubePosition.current = cubePosition;
-        prevCubeQuaternion.current = cubeQuaternion;
-    }, [cubePosition, cubeQuaternion, updateLight]);
-
-    return (
-        <>
-            <ambientLight intensity={1.0} />
-            <spotLight
-                position={[0, 0, 0]}
-                angle={0.3}
-                penumbra={1}
-                intensity={2}
-                castShadow
-                shadow-mapSize-width={256}
-                shadow-mapSize-height={256}
-                ref={ref}
-                color={"#ff0000"}
-                distance={10}
-            />
-        </>
+    // サイコロの上面を算出
+    const upFaceNormal = new Vector3(0, 1, 0).applyQuaternion(
+      new Quaternion(...cubeQuaternion)
     );
+
+    // 位置算出
+    const lightOffset = new Vector3(2, 4, 2);
+    const lightPosition = new Vector3()
+      .addVectors(new Vector3(...cubePosition), upFaceNormal)
+      .add(lightOffset);
+
+    ref.current.target.position.set(...cubePosition);
+    ref.current.position.set(...lightPosition.toArray());
+    ref.current.target.updateMatrixWorld();
+  }, [cubePosition, cubeQuaternion]);
+
+  // 最初の一発だけ
+  useEffect(() => {
+    updateLight();
+  }, []);
+
+  useEffect(() => {
+    updateLight();
+
+    prevCubePosition.current = cubePosition;
+    prevCubeQuaternion.current = cubeQuaternion;
+  }, [cubePosition, cubeQuaternion, updateLight]);
+
+  return (
+    <>
+      <ambientLight intensity={1.0} />
+      <spotLight
+        position={[0, 0, 0]}
+        angle={0.3}
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        ref={ref}
+        color={"#ff0000"}
+        distance={10}
+      />
+    </>
+  );
 };
 
 export default Lights;
--- src/components/GL/MyGL.tsx
+++ src/components/GL/MyGL.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls, Stats } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { Suspense } from "react";
-import { Debug, Physics } from "@react-three/cannon";
+import { Physics } from "@react-three/cannon";
 import { Color } from "three";
 import useMode from "@hooks/useMode";
 import Plane from "./Plane";
@@ -12,48 +12,38 @@ import Lights from "./Lights";
 import React from "react";
 
 export default function MyGL() {
-    const mode = useMode();
+  const mode = useMode();
 
-    return (
-        <Suspense fallback={null}>
-            <Canvas
-                shadows
-                gl={{ alpha: false }}
-                camera={{ position: [-5, 10, 5], fov: 50 }}
-                onCreated={({ scene }) =>
-                    (scene.background = new Color(
-                        mode === "dark"
-                            ? darkParams.bgColor
-                            : lightParams.bgColor
-                    ))
-                }
-            >
-                <color
-                    attach="background"
-                    args={[
-                        mode === "dark"
-                            ? darkParams.bgColor
-                            : lightParams.bgColor,
-                    ]}
-                />
-                <Environment preset="sunset" />
-                <Lights />
+  return (
+    <Suspense fallback={null}>
+      <Canvas
+        shadows
+        gl={{ alpha: false }}
+        camera={{ position: [-5, 10, 5], fov: 50 }}
+        onCreated={({ scene }) =>
+          (scene.background = new Color(
+            mode === "dark" ? darkParams.bgColor : lightParams.bgColor
+          ))
+        }
+      >
+        <color
+          attach="background"
+          args={[mode === "dark" ? darkParams.bgColor : lightParams.bgColor]}
+        />
+        <Environment preset="sunset" />
+        <Lights />
 
-                <Physics broadphase="SAP">
-                    {/* <Debug color="red" scale={1.1}> */}
-                    <Plane
-                        color={
-                            mode === "dark"
-                                ? darkParams.bgColor
-                                : lightParams.bgColor
-                        }
-                    />
-                    <Cube position={[0, 3, 0]} mode={mode} />
-                    {/* </Debug> */}
-                </Physics>
-                {/* <OrbitControls makeDefault />
+        <Physics broadphase="SAP">
+          {/* <Debug color="red" scale={1.1}> */}
+          <Plane
+            color={mode === "dark" ? darkParams.bgColor : lightParams.bgColor}
+          />
+          <Cube position={[0, 3, 0]} mode={mode} />
+          {/* </Debug> */}
+        </Physics>
+        {/* <OrbitControls makeDefault />
                 <Stats /> */}
-            </Canvas>
-        </Suspense>
-    );
+      </Canvas>
+    </Suspense>
+  );
 }
