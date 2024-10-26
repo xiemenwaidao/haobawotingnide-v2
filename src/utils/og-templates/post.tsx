b5560acf109a19f230208329b0985c7a2861528d
xiemenwaidao <menxielijiao@gmail.com>
2024-10-26 11:59:35 +0900

    💄 ogimageのスタイルをv1のものに置き換え

--- src/utils/og-templates/post.tsx
+++ src/utils/og-templates/post.tsx
@@ -7,90 +7,52 @@ export default async (post: CollectionEntry<"blog">) => {
   return satori(
     <div
       style={{
-        background: "#fefbfb",
-        width: "100%",
-        height: "100%",
         display: "flex",
-        alignItems: "center",
         justifyContent: "center",
+        alignItems: "center",
+        width: "1200px",
+        height: "630px",
+        backgroundColor: "#c4b98d",
       }}
     >
       <div
         style={{
-          position: "absolute",
-          top: "-1px",
-          right: "-1px",
-          border: "4px solid #000",
-          background: "#ecebeb",
-          opacity: "0.9",
-          borderRadius: "4px",
+          background: "#ffffff",
+          width: "1140px",
+          height: "570px",
           display: "flex",
-          justifyContent: "center",
-          margin: "2.5rem",
-          width: "88%",
-          height: "80%",
-        }}
-      />
-
-      <div
-        style={{
-          border: "4px solid #000",
-          background: "#fefbfb",
-          borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
-          margin: "2rem",
-          width: "88%",
-          height: "80%",
+          flexFlow: "column",
         }}
       >
         <div
           style={{
+            height: "70%",
+            width: "100%",
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            margin: "20px",
-            width: "90%",
-            height: "90%",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#a33206",
+            fontSize: "40px",
+            fontWeight: "bold",
+            padding: "10px",
           }}
         >
-          <p
+          {post.data.title}
+        </div>
+        <div
+          style={{
+            height: "30%",
+            width: "100%",
+            display: "flex",
+          }}
+        >
+          <img
+            src="https://haobawotingnide.xyz/ogImage.png"
             style={{
-              fontSize: 72,
-              fontWeight: "bold",
-              maxHeight: "84%",
-              overflow: "hidden",
+              margin: "auto",
+              height: "100%",
             }}
-          >
-            {post.data.title}
-          </p>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              marginBottom: "8px",
-              fontSize: 28,
-            }}
-          >
-            {/* <span>
-              by{" "}
-              <span
-                style={{
-                  color: "transparent",
-                }}
-              >
-                "
-              </span>
-              <span style={{ overflow: "hidden", fontWeight: "bold" }}>
-                {post.data.author}
-              </span>
-            </span> */}
-
-            <span style={{ overflow: "hidden", fontWeight: "bold" }}>
-              {SITE.title}
-            </span>
-          </div>
+          />
         </div>
       </div>
     </div>,
